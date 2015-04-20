14befd187298b29e457ff7ca06227ced7a1faf63
metamaker <guru@goblinov.net>
2015-04-20 21:24:29 +0300

    CMake and Bii configs update

--- cmake/Build.cmake
+++ cmake/Build.cmake
@@ -9,9 +9,4 @@ FILE(GLOB PROJECT_SOURCE_FILES source/*.cpp)
 find_package(curl REQUIRED)
 
 add_executable(w3-patch-downloader ${PROJECT_SOURCE_FILES})
-
-if (CURL_FOUND)
-    target_link_libraries(w3-patch-downloader curl)
-else()
-    target_link_libraries(w3-patch-downloader lasote_curl)
-endif()
+target_link_libraries(w3-patch-downloader curl)
